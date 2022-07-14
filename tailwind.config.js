98d3694ec06357e49db17f22f563a894f41690bb
Calven Wong <calven.wong@grabtaxi.com>
2022-07-14 22:53:58 +0800

    added slider section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./main.js",
   ],  theme: {
     extend: {
+      screens: {
+        xs: "425px"
+      },
       colors: {
         bkg: "#171819",
         muted: "#C9CBCF",
