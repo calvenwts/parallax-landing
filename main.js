16a7db8f3805d0235f905752f91f43e4e4f7be25
Calven Wong <calven.wong@grabtaxi.com>
2022-07-23 10:34:47 +0800

    add parallax effect to scroll

--- main.js
+++ main.js
@@ -88,3 +88,26 @@ async function handleFormSubmit(e) {
 
 // event listen form submit
 contactForm.addEventListener('submit', handleFormSubmit);
+
+// Fade up observer
+function fadeUpObserverCallback(elsToWatch){
+    elsToWatch.forEach((el) => {
+        if(el.isIntersecting){
+            el.target.classList.add('faded');
+            fadeUpObserver.unobserve(el.target)
+            el.target.addEventListener("transitioned", () => {
+                el.target.classList.remove('fade-up', 'faded')
+            }, {once: true})
+        }
+    })
+}
+
+const  fadeUpObserverOptions = {
+    threshold: .6,
+}
+
+const fadeUpObserver = new IntersectionObserver(fadeUpObserverCallback, fadeUpObserverOptions)
+
+document.querySelectorAll('.fade-up').forEach((item) => {
+    fadeUpObserver.observe(item);
+})
